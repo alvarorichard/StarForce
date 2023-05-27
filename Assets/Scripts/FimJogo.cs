761562de0448978ee8f365f69b5d05d9095df04b
alvarorichard <alvaro10048@protonmail.com>
2023-05-27 02:55:09 -0300

    Melhor pontuação

--- Assets/Scripts/FimJogo.cs
+++ Assets/Scripts/FimJogo.cs
@@ -7,11 +7,13 @@ using UnityEngine.SceneManagement;
 public class FimJogo : MonoBehaviour
 {
     public Text textoPontuacao;
+    public Text textoMelhorPontuacao;
 
     public void Exibir(){
         this.gameObject.SetActive(true);
         this.textoPontuacao.text =  (ControladorPontuacao.Pontuacao +"x");
-
+        
+        this.textoMelhorPontuacao.text = ControladorPontuacao.MelhorPontuacao.ToString();
         Debug.Log("Melhor pontuação: " + ControladorPontuacao.MelhorPontuacao);
         //pausa o jogo
         Time.timeScale = 0;
