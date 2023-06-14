08d371fe1d57cdce86a097a439bcb528613d0581
alvarorichard <alvaro10048@protonmail.com>
2023-06-14 16:59:31 -0300

    Power-Up fix autohide and delete

--- Assets/Scripts/PowerUp/PowerUpColetavel.cs
+++ Assets/Scripts/PowerUp/PowerUpColetavel.cs
@@ -4,76 +4,77 @@ using UnityEngine;
 
 public abstract class PowerUpColetavel : MonoBehaviour
 {
-   [SerializeField]
-   private SpriteRenderer  spriteRenderer;
+    [SerializeField]
+    private SpriteRenderer spriteRenderer;
 
-  [SerializeField]
-   private float intervaloTempoAntesAutoDestruir;
+    [SerializeField]
+    private float intervaloTempoAntesAutoDestruir;
 
-   private float contagemTempoAntesAutoDestruir;
-   private bool autodestruindo;
+    private float contagemTempoAntesAutoDestruir;
+    private bool autodestruindo;
 
-   [SerializeField]
-   private float intervaloTempoEntrePiscadas;
+    [SerializeField]
+    private float intervaloTempoEntrePiscadas;
 
-   [SerializeField]
-   private int quantidadeTotalPiscadas;
+    [SerializeField]
+    private int quantidadeTotalPiscadas;
 
-   [SerializeField]
+    [SerializeField]
     private float reducaoTempoEntrePiscadas;
-   
 
+    private float tempoDesaparecer;
 
-   public  void Start() {
-    this.autodestruindo = false;
-    this.contagemTempoAntesAutoDestruir = 0;
-    
-   }
-
-   public void Update() {
-    if(!this.autodestruindo){
-      
-     this.contagemTempoAntesAutoDestruir += Time.deltaTime;
-     if(this.contagemTempoAntesAutoDestruir >= this.intervaloTempoAntesAutoDestruir){
-     IniciarAutoDestruicao();
-     }
-   }
+    public void Start()
+    {
+        this.autodestruindo = false;
+        this.contagemTempoAntesAutoDestruir = 0;
+        this.tempoDesaparecer = intervaloTempoAntesAutoDestruir;
     }
 
-   public abstract EfeitoPowerUp EfeitoPowerUp {get;}
+    public void Update()
+    {
+        if (!this.autodestruindo)
+        {
+            this.contagemTempoAntesAutoDestruir += Time.deltaTime;
+            if (this.contagemTempoAntesAutoDestruir >= this.intervaloTempoAntesAutoDestruir)
+            {
+                IniciarAutoDestruicao();
+            }
+        }
+        else
+        {
+            tempoDesaparecer -= Time.deltaTime;
+            if (tempoDesaparecer <= 0)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
 
-   public void Coletar(){
-    Destroy(this.gameObject);
-   }
+    public abstract EfeitoPowerUp EfeitoPowerUp { get; }
 
-   private void IniciarAutoDestruicao(){
-   this.autodestruindo = true;
-    StartCoroutine(Autodestruir());
+    public void Coletar()
+    {
+        Destroy(this.gameObject);
+    }
 
-   
+    private void IniciarAutoDestruicao()
+    {
+        this.autodestruindo = true;
+        StartCoroutine(Autodestruir());
+    }
 
-   }
+    private IEnumerator Autodestruir()
+    {
+        int contadorPiscadas = 0;
+        do
+        {
+            this.spriteRenderer.enabled = !this.spriteRenderer.enabled;
+            yield return new WaitForSeconds(this.intervaloTempoEntrePiscadas);
+            contadorPiscadas++;
+            this.intervaloTempoEntrePiscadas -= contadorPiscadas * this.reducaoTempoEntrePiscadas;
+        } while (contadorPiscadas < this.quantidadeTotalPiscadas);
 
-   private IEnumerator Autodestruir(){
-    int contadorPiscadas = 0;
-   do{
-      
-
-     this.spriteRenderer.enabled = !this.spriteRenderer.enabled;
-// esperar um intervalo de tempo 
-
-  yield return new WaitForSeconds(this.intervaloTempoEntrePiscadas);
-      contadorPiscadas++;
-
-  this.intervaloTempoEntrePiscadas -= contadorPiscadas * this.reducaoTempoEntrePiscadas;
-
-
-   }while(contadorPiscadas < this.quantidadeTotalPiscadas);
-   Destroy(this.gameObject);
-
- }
-
+        Destroy(this.gameObject);
+    }
 }
-
-
-
