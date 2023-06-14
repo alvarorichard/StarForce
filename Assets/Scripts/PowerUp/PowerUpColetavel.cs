a131b04e7bd5849e6e1edf8cbe99e13ae7e14d7c
alvarorichard <alvaro10048@protonmail.com>
2023-06-13 23:47:59 -0300

    powerup auto delete

--- Assets/Scripts/PowerUp/PowerUpColetavel.cs
+++ Assets/Scripts/PowerUp/PowerUpColetavel.cs
@@ -4,11 +4,32 @@ using UnityEngine;
 
 public abstract class PowerUpColetavel : MonoBehaviour
 {
-   
+  [SerializedField]
+   private float intervaloTempoAntesAutoDestruir;
+
+   private float contagemTempoAntesAutoDestruir;
+
+
+   public  void Start() {
+    this.contagemTempoAntesAutoDestruir = 0;
+    
+   }
+
+   public void Update() {
+     this.contagemTempoAntesAutoDestruir += Time.deltaTime;
+     if(this.contagemTempoAntesAutoDestruir >= this.intervaloTempoAntesAutoDestruir){
+       Destroy(this.gameObject);
+     }
+   }
+
    public abstract EfeitoPowerUp EfeitoPowerUp {get;}
 
    public void Coletar(){
     Destroy(this.gameObject);
+   }
+
+   private void IniciarAutoDestruicao(){
+     this.contagemTempoAntesAutoDestruir = 0;
    }
 
  }
