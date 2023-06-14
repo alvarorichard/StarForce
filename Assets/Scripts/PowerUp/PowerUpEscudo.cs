052ec9e4013d61dc45526a16931fd3240b171861
alvarorichard <alvaro10048@protonmail.com>
2023-06-14 17:22:01 -0300

    add powerup shield

---
+++ Assets/Scripts/PowerUp/PowerUpEscudo.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpEscudo : PowerUpColetavel
+{
+    
+public override EfeitoPowerUp EfeitoPowerUp{
+    get{
+        return EfeitoPowerUp.Escudo;
+    }
+}
+
+}
