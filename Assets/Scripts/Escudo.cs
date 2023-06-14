8211b7b68381ba9009d0370d65ca5a026509bb34
alvarorichard <alvaro10048@protonmail.com>
2023-06-14 17:14:08 -0300

    add script shield

---
+++ Assets/Scripts/Escudo.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Escudo : MonoBehaviour
+{
+    
+    [SerializeField]
+    [Tooltip("Quantidade de dano que poder ser recebido antes de ser destruído")]
+     private int protecaoTotal;
+///<summary>
+/// Quantidade de dano que poder ser recebido antes de ser destruído
+///</summary>
+     private int protecaoAtual;
+
+     public void Ativar(){
+        this.gameObject.SetActive(true);
+     }
+
+     public void Desativar(){
+        this.gameObject.SetActive(false);
+     }
+
+     public bool Ativo{
+         get{
+             return this.gameObject.activeSelf;
+         }
+     }
+
+     public void ReceberDano(){
+        this.protecaoAtual--;
+        if(this.protecaoAtual <= 0){
+            this.Desativar();
+        }
+     }
+
+}
