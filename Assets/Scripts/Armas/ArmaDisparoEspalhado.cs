9213cfed4fd23763efa9bf828b32cd4b8f8a8007
alvarorichard <alvaro10048@protonmail.com>
2023-06-14 21:24:12 -0300

    first shot

--- Assets/Scripts/Armas/ArmaDisparoEspalhado.cs
+++ Assets/Scripts/Armas/ArmaDisparoEspalhado.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class ArmaDisparoEspalhado : ArmaBasica
 {
    protected override void Atirar(){
-    
+
+    Vector2 posicaoDisparo = this.posicoesDisparo[0].position;
+
+    CriarLaser(posicaoDisparo);
+
    }
    
 }
